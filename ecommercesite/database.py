69a98ffb94e6bd3838a4c162a9f6dedb904b2701
driedwater <210288D@mymail.nyp.edu.sg>
2022-01-09 14:52:55 +0800

    admin login is finally working again

--- ecommercesite/database.py
+++ ecommercesite/database.py
@@ -2,12 +2,14 @@ from ecommercesite import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
-def load_user(id):
-    return Users.query.get(int(id))
+def load_user(user_id):
+    return User.query.get(int(user_id))
+        
 
-
-class User(db.Model):
-    __abstract__ = True
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(20))
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
     username = db.Column(db.String(30), nullable=False, unique=True)
@@ -15,21 +17,34 @@ class User(db.Model):
     password = db.Column(db.String(60), nullable=False)
     role = db.Column(db.String(10), nullable=False, default='user')
 
-class Users(User, UserMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    __mapper_args__ = {
+        'polymorphic_on':type,
+        'polymorphic_identity':'user'
+    }
+
+    def __repr__(self):
+        return f"User( '{self.username}', '{self.email}')"
+
+class Users(User):
     image_file = db.Column(db.String(20), nullable=False, default='defaultpfp.jpg')
+
+    __mapper_args__ = {
+        'polymorphic_identity':'users'
+    }
 
     def __repr__(self):
         return f"User( '{self.username}', '{self.email}')"
 
 
 class Staff(User):
-    __tablename__ = 'staff'
-    id = db.Column(db.Integer, primary_key=True)
+    product = db.relationship('Product', lazy=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity':'staff'
+    }
 
     def __repr__(self):
-        return f"User('{self.username}, '{self.email}')"
+        return f"User('{self.username}', '{self.email}')"
 
 
 class Product(db.Model):
@@ -37,6 +52,6 @@ class Product(db.Model):
     name = db.Column(db.String(100), nullable=False)
     image_file = db.Column(db.String(20), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    staff_id = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable=False)
+    staff_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 db.create_all()
--- ecommercesite/routes.py
+++ ecommercesite/routes.py
@@ -3,7 +3,7 @@ from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from ecommercesite import app, bcrypt, db
 from ecommercesite.forms import LoginForm, RegistrationForm, UpdateUserAccountForm
-from ecommercesite.database import Users
+from ecommercesite.database import Staff, Users, User
 from flask_login import login_user, current_user, logout_user, login_required
 from functools import wraps
 
@@ -28,6 +28,45 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('error/404.html'), 404
 
+#--------------------LOGIN-LOGOUT-REGISTER-PAGE--------------------------#
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            next = request.args.get('next')
+            return redirect(next) if next else redirect(url_for('home'))
+
+        else:
+            flash('Login unsuccessful. Please check email and/or password.', 'danger')
+    return render_template('login.html', title='Login',form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hash_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = Users(first_name=form.first_name.data, last_name=form.last_name.data, username=form.username.data, email=form.email.data, password=hash_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account has been created, you can now login.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 #--------------------USER-PAGE--------------------------#
 
 @app.route('/')
@@ -45,46 +84,11 @@ def about():
 
 @app.route('/services')
 def services():
-    return render_template('home.html')
+    return render_template('service.html', title='Services')
 
 @app.route('/contacts')
 def contacts():
     return render_template('contacts.html', title='Contacts')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = Users.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user)
-            next = request.args.get('next')
-            return redirect(next) if next else redirect(url_for('home'))
-        else:
-            flash('Login unsuccessful. Please check email and/or password.', 'danger')
-    return render_template('login.html', title='Login',form=form)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        print('this code is running')
-        hash_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = Users(first_name=form.first_name.data, last_name=form.last_name.data, username=form.username.data, email=form.email.data, password=hash_pw)
-        db.session.add(user)
-        db.session.commit()
-        flash(f'Account has been created, you can now login.', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
 
 
 def save_picture(form_pic):
